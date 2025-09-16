642119311000bebaeb8ec1bc45e3c4ded4d2c35c
Lucas <lucaslouzano55@gmail.com>
2025-09-15 22:37:08 -0300

    Implementação BusinessException para lançar exceção no método save

--- src/main/java/com/project/loan/config/BusinessException.java
+++ src/main/java/com/project/loan/config/BusinessException.java
@@ -1,2 +1,23 @@
-package com.project.loan.service.impl;public class BusinessException {
+package com.project.loan.config;
+
+public class BusinessException extends Throwable {
+    public BusinessException(String message) {
+        super(message);
+
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BusinessException(Throwable cause) {
+        super(cause);
+    }
+
+    public BusinessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public BusinessException() {
+    }
 }
