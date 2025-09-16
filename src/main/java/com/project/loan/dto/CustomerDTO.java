421e24388b0a2d418aae924202d1b7ed32ce5bf0
Lucas <lucaslouzano55@gmail.com>
2025-09-15 22:33:19 -0300

    implementação usando DTO para desacoplar entidade Customer

--- src/main/java/com/project/loan/dto/CustomerDTO.java
+++ src/main/java/com/project/loan/dto/CustomerDTO.java
@@ -1,2 +1,14 @@
-package com.project.loan.dto;public record CustomerDTO() {
-}
+package com.project.loan.dto;
+import java.time.LocalDate;
+public record CustomerDTO(
+        Long id,
+        String nome,
+        String cpf,
+        String email,
+        String telefone,
+        LocalDate dataNascimento,
+        String endereco,
+        String cidade,
+        String estado,
+        String cep
+) {}
