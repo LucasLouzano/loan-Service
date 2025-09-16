5289f16dcab8c0e3244db74d0e839f08fe0a4854
Lucas <lucaslouzano55@gmail.com>
2025-09-15 22:24:44 -0300

    Implementação da class de configuração bean restTemplate

--- src/main/java/com/project/loan/config/AppConfig.java
+++ src/main/java/com/project/loan/config/AppConfig.java
@@ -1,12 +1,13 @@
 package com.project.loan.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public AppConfig restTemplate() {
+        return new AppConfig();
     }
 
 }
