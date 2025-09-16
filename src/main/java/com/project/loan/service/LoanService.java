fa2ad7d1ee2bf0d30dcb3035f7f18ddedc7ed25e
Lucas <lucaslouzano55@gmail.com>
2025-09-15 22:19:34 -0300

    Ajuste na interface loanService

--- src/main/java/com/project/loan/service/LoanService.java
+++ src/main/java/com/project/loan/service/LoanService.java
@@ -1,6 +1,7 @@
 package com.project.loan.service;
 
 import com.project.loan.model.Loan;
+import com.project.loan.config.BusinessException;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,8 +9,8 @@ import java.util.Optional;
 public interface LoanService {
 
     Loan findByid(Long id);
-    Loan save(Loan loan);
+    Loan save(Loan loan) throws BusinessException;
     List<Loan> findAll();
     Optional<Loan> update(Long id, Loan loan);
-    boolean delete(Long id);
+    void delete(Long id);
 }
