f79e04540a4b165c17a79c4c911b9aff4d788964
Lucas <lucaslouzano55@gmail.com>
2025-09-15 22:39:51 -0300

    Ajuste na class Loan

--- src/main/java/com/project/loan/model/Loan.java
+++ src/main/java/com/project/loan/model/Loan.java
@@ -9,9 +9,6 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "loans")
 public class Loan {
     @Id
@@ -28,4 +25,75 @@ public class Loan {
 
     private LocalDate dataFim;
 
+    @Column(name = "customer_id", nullable = false)
+    private Long customerId;
+
+    public Loan() {
+    }
+
+    public Loan(Long id, BigDecimal valor, BigDecimal taxaJuros, Integer numeroParcelas, LocalDate dataInicio, LocalDate dataFim, Long customerId) {
+        this.id = id;
+        this.valor = valor;
+        this.taxaJuros = taxaJuros;
+        this.numeroParcelas = numeroParcelas;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.customerId = customerId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public BigDecimal getTaxaJuros() {
+        return taxaJuros;
+    }
+
+    public void setTaxaJuros(BigDecimal taxaJuros) {
+        this.taxaJuros = taxaJuros;
+    }
+
+    public Integer getNumeroParcelas() {
+        return numeroParcelas;
+    }
+
+    public void setNumeroParcelas(Integer numeroParcelas) {
+        this.numeroParcelas = numeroParcelas;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
 }
