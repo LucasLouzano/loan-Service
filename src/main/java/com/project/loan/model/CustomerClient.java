5f7bf2763100352ebc656910518c0111910bf166
Lucas <lucaslouzano55@gmail.com>
2025-09-15 22:22:00 -0300

    Implementação do customerClient para buscar informações do spring boot customer

--- src/main/java/com/project/loan/model/CustomerClient.java
+++ src/main/java/com/project/loan/model/CustomerClient.java
@@ -1,2 +1,28 @@
-package com.project.loan.model;public class CustomerClient {
+package com.project.loan.model;
+
+import com.project.loan.dto.CustomerDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class CustomerClient {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public CustomerDTO buscarClientePorId(Long customerId) {
+        try {
+            String url = "http://localhost:8080/customer/" + customerId;
+            ResponseEntity<CustomerDTO> response = restTemplate.getForEntity(url, CustomerDTO.class);
+            return response.getBody();
+        } catch (HttpClientErrorException.NotFound e) {
+            return null;
+        } catch (RestClientException e) {
+            throw new RuntimeException("Erro ao consultar serviço de cliente", e);
+        }
+    }
 }
